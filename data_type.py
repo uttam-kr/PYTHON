4b9390033e1dabbe5e3f9d82bb0a0b751693fadd
uttam-kr <uttamkr1420@gmail.com>
2019-03-04 23:09:46 +0530

    uttam-kr:20190304:Basic data types!

---
+++ data_type.py
@@ -0,0 +1,31 @@
+# Introduction to Datatypes
+# we will name variables as one, two, three...
+
+# number datatype
+
+one = 1   # integer
+print one
+two = 1.2   # float
+print two
+three = 1+2j    # complex number
+print three
+
+# NONE datatype
+four = None
+print four
+
+# string datatype
+five = "We Love Studytonight"
+print five
+
+# list datatype
+six = [1, 2, 4, 8]
+print six
+
+# tuples
+seven = ("a", "e", "i", "o", "u")
+print seven
+
+# dictionary
+eight = {1, 2, 4, 8}
+print eight
