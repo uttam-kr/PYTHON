81cfaa2b702e1a8d5141804a15a8bfc47ef15813
uttam-kr <uttamkr1420@gmail.com>
2019-03-05 14:37:54 +0530

    uttam-kr:20190305:user input!

---
+++ user_input.py
@@ -0,0 +1,9 @@
+import sys
+#inputing a string
+name = input("enter your name:")
+#inputing a number
+age = input("enter your age:")
+print ("your name is : " + name)
+#using str method to convert number into string
+print("And your age is : " + str(age))
+
