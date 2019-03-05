786c94e325be816ffcd41eb78525826468ad556d
uttam-kr <uttamkr1420@gmail.com>
2019-03-05 14:36:14 +0530

    uttam-kr:20190305:Multiple user input on single call!

---
+++ multi_input.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+#Take multiple user input on single call seprated by space & split it into multiple part by split func.
+name,age = input("enter your name and age: ").split()
+print("your name:" +name)
+print("your age:" +age)
