8835435e45aeac66a516575319bf4ec011014f7c
uttam-kr <uttamkr1420@gmail.com>
2019-03-06 13:53:11 +0530

    uttam-kr:20190306:Basic Atm operation code

---
+++ Atm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import math
+
+savingamount = input("Amount you want to deposit. Please enter in digit: ")
+print("you deposited: " +str(savingamount))
+withdrawamount = input("Amount you want to withdraw: ")
+if int(withdrawamount) > int(savingamount):
+	print("insufficient balance")
+else:
+	savingamount = int(savingamount) - int(withdrawamount)
+	print("Your account balance: " + str(savingamount))
+	print("Have a nice day!")
+	
