eaeee9ff41ff3144c698934a41a481e0e2c65f0f
uttam kumar <38645205+uttam-kr@users.noreply.github.com>
2019-09-13 21:56:34 +0530

    Delete commandline_positional.py

--- commandline_positional.py
+++
@@ -1,6 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display square of given number", type=int)
-args = parser.parse_args()
-print(args.square**2)
