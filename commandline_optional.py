f25d5d4cbc6b799aed31878ec5f92485e4708898
uttam kumar <38645205+uttam-kr@users.noreply.github.com>
2019-09-13 21:56:10 +0530

    Delete commandline_optional.py

--- commandline_optional.py
+++
@@ -1,7 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--verbose", help="please enter optional argument to proceed", action="store_true")
-args = parser.parse_args()
-if args.verbose:
-    print(args.verbose)
-
